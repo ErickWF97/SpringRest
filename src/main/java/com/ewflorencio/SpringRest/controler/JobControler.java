d2aa2218689c434c6dd835576029c956e0ef2733
Erick W. Florencio <erick_w.florencio@outlook.com>
2024-11-22 19:47:46 -0300

    feat: implementation JPA and postgres Driver

---
+++ src/main/java/com/ewflorencio/SpringRest/controler/JobControler.java
@@ -0,0 +1,44 @@
+package com.ewflorencio.SpringRest.controler;
+
+import com.ewflorencio.SpringRest.model.JobPost;
+import com.ewflorencio.SpringRest.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/jobPosts")
+public class JobControler {
+
+    @Autowired
+    private JobService service;
+
+    @GetMapping
+    public List<JobPost> getAllJobs(){
+        return service.getAllJobs();
+    }
+
+    @GetMapping("/{postId}")
+    public JobPost getJob(@PathVariable int postId){
+        return service.getJob(postId);
+    }
+
+    @PostMapping
+    public JobPost addPost(@RequestBody JobPost jobPost){
+       service.addJobPost(jobPost);
+       return service.getJob(jobPost.getPostId());
+    }
+
+    @PutMapping
+    public JobPost updateJob(@RequestBody JobPost jobPost){
+        service.updateJob(jobPost);
+        return service.getJob(jobPost.getPostId());
+    }
+
+    @DeleteMapping("/{postId}")
+    public void deleteJob(@PathVariable int postId){
+        service.deleteJob(postId);
+    }
+
+}
---
+++ src/main/java/com/ewflorencio/SpringRest/service/JobService.java
@@ -0,0 +1,37 @@
+package com.ewflorencio.SpringRest.service;
+
+import com.ewflorencio.SpringRest.model.JobPost;
+import com.ewflorencio.SpringRest.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+    @Autowired
+    private JobRepository repository;
+
+
+    //method to return all JobPosts
+    public List<JobPost> getAllJobs() {
+        return repository.findAll();
+    }
+
+    public void addJobPost(JobPost jobPost) {
+        repository.save(jobPost);
+
+    }
+
+    public JobPost getJob(int id) {
+        return repository.findById(id).orElse(new JobPost());
+    }
+
+    public void updateJob(JobPost jobPost) {
+        repository.save(jobPost);
+    }
+
+    public void deleteJob(int postId) {
+        repository.deleteById(postId);
+    }
+}
